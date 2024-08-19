0df66e6625d086822f389cf70d1679cecff3ced8
Christoph Kuri <chkuri@chkuri.de>
2024-08-19 18:11:28 +0200

    update framework

---
+++ FE/src/pages/SearchResults.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Searchresults = () => {
+  return (
+    <div>
+      <h1 className="underline">SearchResults</h1>
+      <p className="text-red-500">Welcome to the SearchResults page!</p>
+    </div>
+  );
+};
+
+export default Searchresults;
