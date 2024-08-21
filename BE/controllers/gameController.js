398f6bd14e8b12f911fb08550e6c5869d9069dd8
Henry Nguyen <henrylinh94@yahoo.de>
2024-08-21 15:00:24 +0200

    Merge with main

--- BE/controllers/gameController.js
+++ BE/controllers/gameController.js
@@ -1,4 +1,5 @@
 import Game from '../models/Game.js';
+import axios from 'axios';
 
 // Diese CRUD Operations später mit den API calls erweitern
 
@@ -50,5 +51,26 @@ exports.deleteGame = async (req, res) => {
     res.status(204).end();
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+
+export const fetchUpcomingGames = async (req, res) => {
+  try {
+      const response = await axios.post('https://api.igdb.com/v4/games', 
+      `fields name,cover.url,first_release_date,genres.name;
+      where first_release_date > ${Math.floor(Date.now() / 1000)};
+      sort first_release_date asc;
+      limit 10;`,
+      {
+          headers: {
+              'Client-ID': process.env.IGDB_CLIENT_ID,
+              'Authorization': `Bearer ${process.env.IGDB_ACCESS_TOKEN}`
+          }
+      });
+
+      res.json(response.data);
+  } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch upcoming games from IGDB' });
   }
 };
