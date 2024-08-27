0f2de290684afac2d6e83ff20f0d71ba3b73d931
Ckurius <chkuri@chkuri.de>
2024-08-27 15:58:08 +0200

    Update index.js
    Cors update

--- BE/index.js
+++ BE/index.js
@@ -11,10 +11,12 @@ import { createWishlist, getWishlistsByUserId, deleteWishlist } from './controll
 
 const app = express();
 app.use(express.json());
-app.use(cors({  
+app.use(cors());
+
+/* {  
   origin: "http://localhost:8080",
   methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+} */
 
 const PORT = process.env.PORT || 8080;
 
