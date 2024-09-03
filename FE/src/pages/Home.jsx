75770079ff6ba37444d545e34db620066f414738
Christoph Kuri <chkuri@chkuri.de>
2024-09-03 14:20:33 +0200

    update slider Home

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -2,21 +2,20 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Home = () => {
-  const [games, setGames] = useState([]); 
+  const [games, setGames] = useState([]);
   const [upcomingGames, setUpcomingGames] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
 
   useEffect(() => {
-     fetchGames(); 
-     fetchUpcomingGames();
-  }, []); 
+    fetchGames();
+    fetchUpcomingGames();
+  }, []);
 
   const fetchGames = async () => {
     try {
       const response = await axios.get('http://localhost:8080/all-games');
-      const results = response.data;
+      const results = response.data.slice(0, 4);
 
       // Hol die Cover für jedes Spiel
       const gamesWithCovers = await Promise.all(
@@ -32,7 +31,7 @@ const Home = () => {
       // Fehlerbehandlung
       console.error('Error fetching games:', error);
       setError('Error fetching games: ' + error.message);
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
@@ -42,15 +41,13 @@ const Home = () => {
         `http://localhost:8080/searchCoverById?id=${game}`
       );
       const result = response.data;
+      console.log('cover ausgabe Home:', result[0]); // Home Cover ?
       return result[0];
     } catch (error) {
       console.error('Fehler beim Abrufen der Covers:', error);
       return null;
     }
   };
-
-
-
 
   const fetchUpcomingGames = async () => {
     try {
@@ -66,38 +63,32 @@ const Home = () => {
         })
       );
 
-
       setUpcomingGames(gamesWithCovers);
       setLoading(false);
     } catch (error) {
       console.error('Error fetching upcoming games:', error);
       setError('Error fetching upcoming games: ' + error.message);
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
-  
-
   if (loading) {
     return (
-      <div className="h-screen p-28 bg-[#141414] text-white">Loading...</div>
+      <div className="flex items-center justify-center min-h-screen bg-[#141414]">
+        <div className="relative w-24 h-24 animate-spin">
+          <div className="absolute inset-0 border-4 border-t-transparent border-white rounded-full animate-[spin_1s_linear_infinite]"></div>
+          <div className="absolute inset-0 border-4 border-t-transparent border-[#1CE0AF] rounded-full animate-[spin_2s_linear_infinite]"></div>
+        </div>
+        <span className="absolute text-white mt-32 text-lg tracking-wide font-bold">
+          NextGameFinder lädt...
+        </span>
+      </div>
     );
   }
 
   if (error) {
     return <div className="p-28 bg-[#141414] text-white h-screen">{error}</div>;
   }
-
-
-
-
-
-
-
-
-
-
-
 
   return (
     <div className="bg-[#141414] min-h-screen w-full dark:text-white">
@@ -119,8 +110,12 @@ const Home = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-14 p-28">
         <div className="relative h-[30vh] w-full flex items-center justify-center bg-cover bg-center">
           <div className="relative z-10 text-center px-4">
-            <h2 className="text-5xl font-bold mb-4 text-primary-500">Game Library</h2>
-            <p className="text-xl font-light">Explore the best games from our collection</p>
+            <h2 className="text-5xl font-bold mb-4 text-primary-500">
+              Game Library
+            </h2>
+            <p className="text-xl font-light">
+              Explore the best games from our collection
+            </p>
           </div>
         </div>
 
@@ -131,7 +126,7 @@ const Home = () => {
             onClick={() =>
               (window.location.href = `/GameDetails?id=${game.id}`)
             }
-            >
+          >
             <div
               className="absolute inset-0 bg-cover bg-center rounded-lg"
               style={{
@@ -161,7 +156,6 @@ const Home = () => {
         ))}
       </div>
 
-
       {/* Upcoming Games Section */}
       <div className="p-28 w-full">
         <h2 className="text-4xl font-bold text-white mb-8">Upcoming Games</h2>
@@ -173,13 +167,15 @@ const Home = () => {
               onClick={() =>
                 (window.location.href = `/GameDetails?id=${game.id}`)
               }
-              >
+            >
               <div
                 className="absolute inset-0 bg-cover bg-center rounded-lg"
-                style={{ backgroundImage: `url(${
-                  game.cover_url ||
-                  'https://www.igdb.com/packs/static/igdbLogo-bcd49db90003ee7cd4f4.svg'
-                })` }} 
+                style={{
+                  backgroundImage: `url(${
+                    game.cover_url ||
+                    'https://www.igdb.com/packs/static/igdbLogo-bcd49db90003ee7cd4f4.svg'
+                  })`,
+                }}
               ></div>
               <div className="absolute inset-0 bg-[#141414] bg-opacity-50 rounded-lg"></div>
               <button
@@ -194,16 +190,13 @@ const Home = () => {
                   {game.name}
                 </h2>
                 <h2 className="relative z-10 text-md font-medium text-center text-[#1CE0AF] pb-4">
-                  Game-Id: {game.id} 
+                  Game-Id: {game.id}
                 </h2>
               </div>
             </div>
           ))}
         </div>
       </div>
-
-
-
     </div>
   );
 };
